562e50e51f2e34c23b2912cf50699f13d8c6f57f
Your Name <you@example.com>
2023-09-30 16:46:36 +0700

    Update

--- src/app/layout.js
+++ src/app/layout.js
@@ -91,8 +91,8 @@ export default function RootLayout({ children }) {
               </Title>
               {children}
               <Footer
-                studentId="650610560"
-                fullName="Chayanin Suatap"
+                studentId="650610818"
+                fullName="Anawat Ratanakitsorn"
                 year="2023"
               />
             </Container>
